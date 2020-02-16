1489e10380c99dd81edfca7bdef95f8731f30cdc
Shahid <shahidzainsz77@gmail.com>
2020-02-16 22:28:33 +0530

    search and sort techniques, along with stack,queues etc

--- recursive.py
+++ recursive.py
@@ -4,4 +4,21 @@ def fibo(n):
     else:
         return fibo(n-1)+fibo(n-2)
 
-print(fibo(5))
+def odd_eve(n):
+    if n==0:
+        return
+    else:
+        odd_eve(n-1)
+        if n%2==0:
+            print("even : ",n)
+        else:
+            print("odd : ",n)
+
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return (n*fact(n-1))
+
+n=int(input("enter the value : "))
+print(fact(n))
